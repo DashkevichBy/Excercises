bf330338088b77b77662f6aecfc848ca2da34c2d
euheny.dashkevich <euheny.dashkevich@wsc-042.icx.local>
2017-01-17 13:18:50 +0300

    fix

--- TutByTest/src/TutByAndGmailTest.java
+++ TutByTest/src/TutByAndGmailTest.java
@@ -6,8 +6,8 @@ public class TutByAndGmailTest {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		RunGmailTest gTest = new RunGmailTest();
-		RunTutByTest tTest = new RunTutByTest();
+		RunGmailTest  gTest = new RunGmailTest();
+		RunTutByTest  tTest = new RunTutByTest();
 		
 		System.setProperty("webdriver.gecko.driver", "./src/Recources/geckodriver.exe");
 
