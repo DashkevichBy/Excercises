466ca2ec3faba88773fd21a4102e180793bd3dac
euheny.dashkevich <euheny.dashkevich@wsc-042.icx.local>
2017-01-20 10:50:36 +0300

    removed unnecessary import

--- ActionsTest/src/EbayDrop/DragAndDrop.java
+++ ActionsTest/src/EbayDrop/DragAndDrop.java
@@ -1,15 +1,13 @@
 package EbayDrop;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 public class DragAndDrop {
 
 	public static void performDragAndDrop(Actions builder, WebElement dragElement, WebElement dropElement) {
 
-				
-	    builder.dragAndDrop(dragElement, dropElement).perform();
+		builder.dragAndDrop(dragElement, dropElement).perform();
 		
 	}
 }
